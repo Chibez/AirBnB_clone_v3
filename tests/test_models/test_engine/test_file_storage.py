a69cd1dcf3652e8df07da4e4a773819a9a636ca8
caleb odobi <odocaleb@gmail.com>
2024-04-29 15:50:31 -0700

    Improve storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -112,4 +112,12 @@ class TestFileStorage(unittest.TestCase):
         string = json.dumps(new_dict)
         with open("file.json", "r") as f:
             js = f.read()
-        self.assertEqual(json.loads(string), json.loads(js))
+self.assertEqual(json.loads(string), json.loads(js))
+
+@unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that get retrieves objects stored in file.json"""
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that count returns the right number of objects in file.json"""
